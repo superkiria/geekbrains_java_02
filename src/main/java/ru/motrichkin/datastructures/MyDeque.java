f31e7a3e606068261250efcc145f04d85d72783f
Kirill Motrichkin <kirill.motrichkin@ya.ru>
2020-02-23 22:21:20 +0300

    Started with deque sculpturing for interface

--- src/main/java/ru/motrichkin/datastructures/MyDeque.java
+++ src/main/java/ru/motrichkin/datastructures/MyDeque.java
@@ -1,6 +1,11 @@
 package ru.motrichkin.datastructures;
 
-public class MyDeque<ItemClass> {
+import java.util.Collection;
+import java.util.Deque;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class MyDeque<ItemClass> implements Deque {
 
     static private int DEFAULT_CAPACITY = 3;
 
@@ -51,10 +56,6 @@ public class MyDeque<ItemClass> {
         return left >= 0 ? left % array.length : array.length + left;
     }
 
-    public int size() {
-        return right - left - 1;
-    }
-
     public void appendRight(ItemClass item) {
         array[realRight()] = item;
         right++;
@@ -68,17 +69,202 @@ public class MyDeque<ItemClass> {
     }
 
     public ItemClass popRight() {
-        right--;
+        if (size() > 0) {
+            right--;
+        }
         ItemClass result = array[realRight()];
         array[realRight()] = null;
         return result;
     }
 
     public ItemClass popLeft() {
-        left++;
+        if (size() > 0) {
+            left++;
+        }
         ItemClass result = array[realLeft()];
         array[realLeft()] = null;
         return result;
+    }
+
+    @Override
+    public void addFirst(Object o) {
+        appendLeft((ItemClass) o);
+    }
+
+    @Override
+    public void addLast(Object o) {
+        appendRight((ItemClass) o);
+    }
+
+    @Override
+    public boolean offerFirst(Object o) {
+        appendLeft((ItemClass) o);
+    }
+
+    @Override
+    public boolean offerLast(Object o) {
+        appendRight((ItemClass) o);
+    }
+
+    @Override
+    public Object removeFirst() {
+        if (size() == 0) {
+            throw new NoSuchElementException("The queue is empty");
+        }
+        return popLeft();
+    }
+
+    @Override
+    public Object removeLast() {
+        if (size() == 0) {
+            throw new NoSuchElementException("The queue is empty");
+        }
+        return popRight();
+    }
+
+    @Override
+    public Object pollFirst() {
+        return popLeft();
+    }
+
+    @Override
+    public Object pollLast() {
+        return popRight();
+    }
+
+    @Override
+    public Object getFirst() {
+        return null;
+    }
+
+    @Override
+    public Object getLast() {
+        return null;
+    }
+
+    @Override
+    public Object peekFirst() {
+        return null;
+    }
+
+    @Override
+    public Object peekLast() {
+        return null;
+    }
+
+    @Override
+    public boolean removeFirstOccurrence(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean removeLastOccurrence(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean add(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean offer(Object o) {
+        return false;
+    }
+
+    @Override
+    public Object remove() {
+        return null;
+    }
+
+    @Override
+    public Object poll() {
+        return null;
+    }
+
+    @Override
+    public Object element() {
+        return null;
+    }
+
+    @Override
+    public Object peek() {
+        return null;
+    }
+
+    @Override
+    public boolean addAll(Collection collection) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public boolean retainAll(Collection collection) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection collection) {
+        return false;
+    }
+
+    @Override
+    public void push(Object o) {
+
+    }
+
+    @Override
+    public Object pop() {
+        return null;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection collection) {
+        return false;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return right - left - 1;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public Iterator iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public Object[] toArray(Object[] objects) {
+        return new Object[0];
+    }
+
+    @Override
+    public Iterator descendingIterator() {
+        return null;
     }
 
 }
